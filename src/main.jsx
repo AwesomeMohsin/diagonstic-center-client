fea94e4005f67650d28c2666b5c6137f05bd208e
The Awesome <mohsindude5@gmail.com>
2023-12-12 12:18:23 +0600

    added messenger text feature

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { RouterProvider } from "react-router-dom";
 import router from './Routes/Route';
 import AuthProvider from './providers/AuthProvider';
 import { HelmetProvider } from 'react-helmet-async';
+import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 import {
   useQuery,
@@ -20,10 +21,15 @@ const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
+
       <AuthProvider>
         <HelmetProvider>
+
           <RouterProvider router={router} />
         </HelmetProvider>
+        <MessengerCustomerChat
+          pageId="194790160378936"
+          appId="143791218624400" />
       </AuthProvider>
     </QueryClientProvider>
   </React.StrictMode>,
